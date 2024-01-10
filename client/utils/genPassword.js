c21eb48d9fbf0f32fe2a3fa234bd966363615c3c
Damion Morgan <only4school5680@gmail.com>
2024-01-10 12:41:19 -0500

    used a npm package and get up genService class

--- client/utils/genPassword.js
+++ client/utils/genPassword.js
@@ -1,27 +1,44 @@
-// All Character types in regex
-const uppercaseRegex = /[A-Z]/;
-const lowercaseRegex = /[a-z]/;
-const specialCharsRegex = /[!@#$%^&*()-_+=\[\]{}|;:,.<>?]/;
-const numbersRegex = /\d/; // or /[0-9]/
 
-// All the characters needed to make a password placed inside an Array
-const alphabetUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-const alphabetLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '|', ';', ':', ',', '.', '/', '<', '>', '?']
-const numbersArray = [0,1, 2, 3, 4, 5, 6, 7, 8, 9,]
+// /* MIGHT JUST USE AN NPM PACKAGE BIG DAWG
+// https://www.npmjs.com/package/generate-password-  OG VERSION
+// https://www.npmjs.com/package/generate-password-browser -- BROWSER SUPPORT
+// */
 
-/* Generating the default password 
-1)Has 50 characters 
-2)Uses all character types
-*/
+import generatePassword from 'generate-password';
 
 /* Function Criteria
-Must be able to generate a password selectivey (adding and excluding character types)
-Returns a password with the given length
-Done randomly
+ Must be able to generate a password selectivey (adding and excluding character types)
+ Returns a password with the given length
+ Done randomly
 */
+class GenService{
 
-/* MIGHT JUST USE AN NPM PACKAGE BIG DAWG
-https://www.npmjs.com/package/generate-password-  OG VERSION
-https://www.npmjs.com/package/generate-password-browser -- BROWSER SUPPORT
-*/
+    getDefault(){
+ // /* Generating the default password 
+// 1)Has 50 characters 
+// 2)Uses all character types
+// */
+    const defaultpassword = generatePassword.generate({
+    length: 12,
+    numbers: true,
+    symbols: true,
+    uppercase: true,
+    lowercase:true,
+    strict:true
+  });
+    return defaultpassword
+    }
+
+    genUserPsw(length,upper,lower,num,spec){
+    const userPsw=generatePassword.generate({
+    length:length,
+    numbers: num,
+    symbols: spec,
+    uppercase: lower,
+    lowercase:upper,
+  });
+    }
+
+}
+
+export default new GenService()
