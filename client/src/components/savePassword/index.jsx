8550366acefcc2e2dfc370be348fb02116f06d1c
Damion Morgan <only4school5680@gmail.com>
2024-01-10 22:29:15 -0500

    user is now notified when they try to save password but not loggedin

--- client/src/components/savePassword/index.jsx
+++ client/src/components/savePassword/index.jsx
@@ -1,29 +1,33 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 function SavePassword(){
-return(
-    <>
+  const [show, setShow] = useState(false);
 
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+return (
+  <>
+      <Button variant="primary" onClick={handleShow}>
+        Save Password
+      </Button>
 
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
-)
+  );
+
 }
 export default SavePassword
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,26 +1,64 @@
-import Auth from "../../utils/auth"
-import SavePassword from "../components/savePassword"
-import GenPassword from "../components/genPassword"
-function HomePage(){
+import Auth from "../../utils/auth";
+import SavePassword from "../components/savePassword";
+import GenPassword from "../components/genPassword";
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
 
-const onSaveClick=()=>{
+function HomePage() {
+  const [showToast, setShowToast] = useState(false);
+  const [position, setPosition] = useState('top-start');
 
-}
-    return(
+  const onSaveClick = () => {
+    setShowToast(!showToast);
+    // Your logic for save click goes here
+  };
+
+  const toggleToast = () => setShowToast(!showToast);
+
+  return (
+    <>
+      <GenPassword></GenPassword>
+
+      {!Auth.loggedIn() ? (
         <>
-<GenPassword></GenPassword>
+          <button onClick={onSaveClick} className="mb-2">
+            Save Password
+          </button>
 
-  {          !Auth.loggedIn()?(
-     <button onClick={onSaveClick}>Must be logged in to Save Password</button>
-    //  add some css later
-            ):(
-     <SavePassword></SavePassword>
-            )}
-       
-
-        
+          {showToast && (
+            <div>
+              <ToastContainer
+                className="p-3"
+                position={position}
+                style={{ zIndex: 1 }}
+              >
+                <Toast onClose={toggleToast}>
+                  <Toast.Header closeButton={false}>
+                    <img
+                      src="holder.js/20x20?text=%20"
+                      className="rounded me-2"
+                      alt=""
+                    />
+                    <strong className="me-auto">Error</strong>
+                    <button
+                      type="button"
+                      className="btn-close"
+                      onClick={toggleToast}
+                    ></button>
+                  </Toast.Header>
+                  <Toast.Body>Must have an account to save password</Toast.Body>
+                </Toast>
+              </ToastContainer>
+            </div>
+          )}
+        </>
+      ) : (
+        <SavePassword></SavePassword>
+      )}
     </>
-    )
+  );
 }
 
-export default HomePage
+export default HomePage;
